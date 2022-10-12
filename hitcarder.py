08522bcd7e8a43152ca6174d4f1061dec307ca8b
yinqiaozhangZJU <113837468+yinqiaozhangZJU@users.noreply.github.com>
2022-10-12 17:10:28 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -1,56 +1,49 @@
 # -*- coding: utf-8 -*-
-import requests
+
+# 打卡脚修改自ZJU-nCov-Hitcarder的开源代码，感谢这位同学开源的代码
+import message
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+import requests
 import json
 import re
-import time
 import datetime
-import os
+import time
 import sys
-import message
-import ddddocr
-
+# import ddddocr
+import os
 
 class HitCarder(object):
-    """Hit carder class
+    """Hit card class
 
     Attributes:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
-        login_url: (str) 登录url
-        base_url: (str) 打卡首页url
-        save_url: (str) 提交打卡url
+        LOGIN_URL: (str) 登录url
+        BASE_URL: (str) 打卡首页url
+        SAVE_URL: (str) 提交打卡url
+        HEADERS: (dir) 请求头
         sess: (requests.Session) 统一的session
     """
-
+    LOGIN_URL = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
+    BASE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
+    SAVE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+ #   captcha_url = "https://healthreport.zju.edu.cn/ncov/wap/default/code"
+    HEADERS = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+    }
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
-        self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
-        self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
-        self.captcha_url = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
         self.sess = requests.Session()
-        self.sess.keep_alive = False
-        retry = Retry(connect=3, backoff_factor=0.5)
-        adapter = HTTPAdapter(max_retries=retry)
-        self.sess.mount('http://', adapter)
-        self.sess.mount('https://', adapter)
-        # ua = UserAgent()
-        # self.sess.headers['User-Agent'] = ua.chrome
-        self.sess.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
 
     def login(self):
         """Login to ZJU platform"""
-        time.sleep(1)
-        res = self.sess.get(self.login_url)
+        res = self.sess.get(self.LOGIN_URL, headers=self.HEADERS)
         execution = re.search(
             'name="execution" value="(.*?)"', res.text).group(1)
-        time.sleep(1)
         res = self.sess.get(
-            url='https://zjuam.zju.edu.cn/cas/v2/getPubKey').json()
+            url='https://zjuam.zju.edu.cn/cas/v2/getPubKey', headers=self.HEADERS).json()
         n, e = res['modulus'], res['exponent']
         encrypt_password = self._rsa_encrypt(self.password, e, n)
 
@@ -60,8 +53,7 @@ class HitCarder(object):
             'execution': execution,
             '_eventId': 'submit'
         }
-        time.sleep(1)
-        res = self.sess.post(url=self.login_url, data=data)
+        res = self.sess.post(url=self.LOGIN_URL, data=data, headers=self.HEADERS)
 
         # check if login successfully
         if '统一身份认证' in res.content.decode():
@@ -69,40 +61,24 @@ class HitCarder(object):
         return self.sess
 
     def post(self):
-        """Post the hit card info."""
-        time.sleep(1)
-        res = self.sess.post(self.save_url, data=self.info)
+        """Post the hitcard info"""
+        res = self.sess.post(self.SAVE_URL, data=self.info, headers=self.HEADERS)
         return json.loads(res.text)
 
     def get_date(self):
-        """Get current date."""
-        today = datetime.datetime.utcnow() + datetime.timedelta(hours=+8)
+        """Get current date"""
+        today = datetime.date.today()
         return "%4d%02d%02d" % (today.year, today.month, today.day)
 
-    def check_form(self):
-        """Get hitcard form, compare with old form """
-        res = self.sess.get(self.base_url)
-        html = res.content.decode()
-
-        try:
-            new_form = re.findall(r'<ul>[\s\S]*?</ul>', html)[0]
-        except IndexError as _:
-            raise RegexMatchError('Relative info not found in html with regex')
-
-        with open("form.txt", "r", encoding="utf-8") as f:
-            if new_form == f.read():
-                return True
-        #with open("form.txt", "w", encoding="utf-8") as f:
-        #     f.write(new_form)
-        return False
-
     def get_info(self, html=None):
-        """Get hit card info, which is the old info with updated new time."""
+        """Get hitcard info, which is the old info with updated new time."""
         if not html:
-            time.sleep(1)
-            res = self.sess.get(self.base_url)
+            res = self.sess.get(self.BASE_URL, headers=self.HEADERS)
             html = res.content.decode()
-
+            # 新建ocr，并读取验证码进行识别
+     #       ocr = ddddocr.DdddOcr(old=True)
+     #       resp = self.sess.get(self.captcha_url, headers=self.HEADERS)
+     #       captcha = ocr.classification(resp.content)
         try:
             old_infos = re.findall(r'oldInfo: ({[^\n]+})', html)
             if len(old_infos) != 0:
@@ -110,41 +86,49 @@ class HitCarder(object):
             else:
                 raise RegexMatchError("未发现缓存信息，请先至少手动成功打卡一次再运行脚本")
 
-            def_info = json.loads(re.findall(r'def = ({[^\n]+})', html)[0])
-            magic_code = re.findall(
-                r'"([0-9a-z]{32})": "([0-9]{10})","([0-9a-z]{32})":"([0-9a-z]{32})"', html)[0]
-            magic_code_group = {
-                magic_code[0]: magic_code[1],
-                magic_code[2]: magic_code[3]
-            }
+            new_info_tmp = json.loads(re.findall(r'def = ({[^\n]+})', html)[0])
+            new_id = new_info_tmp['id']
+            name = re.findall(r'realname: "([^\"]+)",', html)[0]
+            number = re.findall(r"number: '([^\']+)',", html)[0]
+        except IndexError:
+            raise RegexMatchError('Relative info not found in html with regex')
+        except json.decoder.JSONDecodeError:
+            raise DecodeError('JSON decode error')
 
-        except IndexError as err:
-            raise RegexMatchError(
-                'Relative info not found in html with regex: ' + str(err))
-        except json.decoder.JSONDecodeError as err:
-            raise DecodeError('JSON decode error: ' + str(err))
-
-        new_info = def_info.copy()
-        new_info.update(magic_code_group)
-        ocr = ddddocr.DdddOcr()
-        resp = self.sess.get(self.captcha_url)
-        # form change
-        new_info['szgjcs'] = ""
-        new_info['zgfx14rfhsj'] = ""
-        new_info['geo_api_info'] = old_info['geo_api_info'] # 定位
-        new_info['address'] = old_info['address']
-        new_info['area'] = old_info['area']
-        new_info['city'] = old_info['city']
-        new_info['ismoved'] = 0
-        new_info['sfzx'] = old_info['sfzx'] # 在校
-        new_info['sfymqjczrj'] = old_info['sfymqjczrj'] # 同住人发热
-        new_info['sfqrxxss'] = 1 # 属实
+        new_info = old_info.copy()
+        new_info['id'] = new_id
+        new_info['name'] = name
+        new_info['number'] = number
+        new_info["date"] = self.get_date()
+        new_info["created"] = round(time.time())
+        new_info["address"] = "浙江省杭州市西湖区"
+        new_info["area"] = "浙江省 杭州市 西湖区"
         new_info['campus'] = '紫金港校区' #校区
-        new_info['internship'] = old_info['internship'] # 实习
-        #new_info['verifyCode'] =  ocr.classification(resp.content)#验证码
+        new_info["province"] = new_info["area"].split(' ')[0]
+        new_info["city"] = new_info["area"].split(' ')[1]
+        # form change
+        new_info['jrdqtlqk[]'] = 0
+        new_info['jrdqjcqk[]'] = 0
+        new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
+        new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
+        new_info['sfqrxxss'] = 1    # 是否确认信息属实
+        new_info['jcqzrq'] = ""
+        new_info['gwszdd'] = ""
+        new_info['szgjcs'] = ""
+        
+        # add in 2022.07.08
+        new_info['sfymqjczrj'] = 2  #同住人员是否发热
+        new_info['ismoved'] = 4     #是否有离开
+        new_info['internship'] = 3  #是否进行实习
+        new_info['sfcxzysx'] = 2    #是否涉及疫情管控
+        
+   #     new_info['verifyCode'] = captcha
+        # 2021.08.05 Fix 2
+        magics = re.findall(r'"([0-9a-f]{32})":\s*"([^\"]+)"', html)
+        for item in magics:
+            new_info[item[0]] = item[1]
 
         self.info = new_info
-        # print(json.dumps(self.info))
         return new_info
 
     def _rsa_encrypt(self, password_str, e_str, M_str):
@@ -173,6 +157,7 @@ class DecodeError(Exception):
 
 
 def main(username, password):
+
     """Hit card process
 
     Arguments:
@@ -193,13 +178,6 @@ def main(username, password):
         return 1, '打卡登录失败：' + str(err)
 
     try:
-        ret = hit_carder.check_form()
-        if not ret:
-            return 2, '打卡信息已改变，请手动打卡'
-    except Exception as err:
-        return 1, '获取信息失败，请手动打卡: ' + str(err)
-
-    try:
         hit_carder.get_info()
     except Exception as err:
         return 1, '获取信息失败，请手动打卡: ' + str(err)
@@ -208,9 +186,9 @@ def main(username, password):
         res = hit_carder.post()
         print(res)
         if str(res['e']) == '0':
-            return 0, 'shui今日份打卡成功~'
+            return 0, '打卡成功'
         elif str(res['m']) == '今天已经填报了':
-            return 0, 'shui今日已经打卡了呢'
+            return 0, '今天已经打卡'
         else:
             return 1, '打卡失败'
     except:
